9fada5a837155ea212c341bc11434f75254a3dbd
Brayan Gutierrez Dinarte <38222878+BrayanD17@users.noreply.github.com>
2024-11-08 13:40:54 -0600

    Update

--- Compilador-MiniPython/Compilador-MiniPython/obj/Debug/net8.0/CompiladorMini-Python.AssemblyInfo.cs
+++ Compilador-MiniPython/Compilador-MiniPython/obj/Debug/net8.0/CompiladorMini-Python.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CompiladorMini-Python")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0eb09a7330de6581e069d34512f7b8f04c141d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b75e784ece33a22c7c00a5cc7d26dc800472f50")]
 [assembly: System.Reflection.AssemblyProductAttribute("CompiladorMini-Python")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CompiladorMini-Python")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
